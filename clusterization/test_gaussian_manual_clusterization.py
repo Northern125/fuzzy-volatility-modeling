0d24aa69991316069de4f86bed44ce60e88a6c6e
Vladimir Sviyazov <sva125125@gmail.com>
2021-10-13 18:08:25 +0300

    [Feature] Extended a unit test for Gaussian membership function

--- clusterization/test_gaussian_manual_clusterization.py
+++ clusterization/test_gaussian_manual_clusterization.py
@@ -1,5 +1,5 @@
 import unittest
-from numpy import array
+from numpy import array, exp
 from math import pi
 
 from gaussian import calc_gaussian_membership_degrees
@@ -16,6 +16,21 @@ class TestGaussianMembershipFunction(unittest.TestCase):
         should_be = 1 / ((2 * pi) ** .5)
 
         self.assertEqual(result, should_be)
+
+    def test_1d_3_clusters(self):
+        input_data = array([0])
+        centers = array([[-1], [0], [1]])
+        cov_matrices = array([[[1]], [[1]], [[1]]])
+
+        result = calc_gaussian_membership_degrees(input_data, centers, cov_matrices)
+
+        coeff = 1 / ((2 * pi) ** .5)
+        e_to_one_half = exp(-.5)
+        should_be = [coeff * e_to_one_half, coeff, coeff * e_to_one_half]
+
+        assertion = (result == should_be).all()
+
+        self.assertTrue(assertion)
 
 
 if __name__ == '__main__':
