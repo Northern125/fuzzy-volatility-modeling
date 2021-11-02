b237d14433b43d088c48eef6f3ac92dc85cb24d3
Vladimir Sviyazov <sva125125@gmail.com>
2021-11-02 23:24:14 +0300

    [Featured] Restructured FuzzyVolatilityModel
    `fit()` now only fits parameters, but doesn't produce outputs. For calculating outputs functions `_calc_local_models_forecasts()` and `forecast()` are implemented

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import logging
+import json
+
+from arch import arch_model
 from pandas import Series, DataFrame
-from numpy import array
 
 from clusterization import cluster_data
-from local_models import apply_local_model
 from rules_related import combine_rules_outputs
 
 module_logger = logging.getLogger('model')
@@ -49,6 +50,10 @@ class FuzzyVolatilityModel:
         self.hist_output = Series(dtype=float).copy()
         self.current_output = None
 
+        # garch objects
+        self.garch_models = []
+        self.fitted_garch_models = []
+
     def fit(self, train_data: Series = None):
         if train_data is not None:
             self.train_data = train_data.copy()
@@ -72,24 +77,47 @@ class FuzzyVolatilityModel:
         self._clusters_parameters_hist.append(self.clusters_parameters_current)
         self._membership_degrees_hist.append(self.membership_degrees_current)
 
-        # running local models for each rule
-        self.logger.debug('Starting to run local model for each rule')
+        # fitting local models within each rule
+        self.logger.debug('Starting to fit local model within each rule')
 
-        self.rules_outputs_current = []
-        for i in range(n_clusters):
-            rule_output = apply_local_model(self.train_data,
-                                            method=self.local_method,
-                                            parameters=self.local_method_parameters,
-                                            forecast_horizon=1)['forecast'][0]  # yes, hardcode:
-            # we only forecast for the next day, it is a common practice
-            self.rules_outputs_current.append(rule_output)
+        # self.rules_outputs_current = []
+        for _ in range(n_clusters):
+            # rule_output = apply_local_model(self.train_data,
+            #                                 method=self.local_method,
+            #                                 parameters=self.local_method_parameters,
+            #                                 forecast_horizon=1)['forecast'][0]  # yes, hardcode:
+            # # we only forecast for the next day, it is a common practice
+            # self.rules_outputs_current.append(rule_output)
 
-        self.rules_outputs_current = array(self.rules_outputs_current)
+            p = self.local_method_parameters['p']
+            q = self.local_method_parameters['q']
+            mean = self.local_method_parameters['mean']
+            dist = self.local_method_parameters['dist']
 
-        self.logger.debug(f'Local model runs for each rule are completed. rules_outputs_current: '
-                          f'{self.rules_outputs_current}')
+            model = arch_model(self.train_data, mean=mean, vol='GARCH', p=p, q=q, dist=dist)
+            fitted = model.fit()
+
+            self.garch_models.append(model)
+            self.fitted_garch_models.append(fitted)
+
+        # self.rules_outputs_current = array(self.rules_outputs_current)
+
+        self.logger.debug('Local model fittings for each rule are completed')
 
         self._rules_outputs_hist.append(self.rules_outputs_current)
+
+    def _calc_local_models_forecasts(self, horizon=1):
+        rules_outputs = []
+
+        for model in self.fitted_garch_models:
+            rule_output = model.forecast(reindex=False, horizon=horizon).variance.iloc[0]
+            rules_outputs.append(rule_output)
+
+        return rules_outputs
+
+    def forecast(self):
+        rules_outputs = self._calc_local_models_forecasts(horizon=1)
+        self.rules_outputs_current = [output.loc['h.1'] for output in rules_outputs]
 
         # aggregating rules outputs to a single output
         self.logger.debug('Starting to aggregate all rules outputs to a single one')
@@ -102,7 +130,7 @@ class FuzzyVolatilityModel:
         self.train_data.loc[observation_date] = observation
         self.fit()
 
-    def forecast(self, test_data: Series):
+    def feed_daily_data(self, test_data: Series):
         for date in test_data.index:
             observation = test_data.loc[date]
             self.push(observation, date)
