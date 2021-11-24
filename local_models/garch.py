7e590f48422058b09e62f5616c1ba76817695894
Vladimir Sviyazov <sva125125@gmail.com>
2021-11-24 20:28:39 +0300

    [Feature] Expanded calculation of conditional variance for fuzzy GARCH

--- local_models/garch.py
+++ local_models/garch.py
@@ -23,8 +23,20 @@ def calc_fuzzy_ht(alpha_0, alpha, beta, y_squared, h, weights):
     return result
 
 
-def calc_cond_var(alpha_0, alpha, beta, y, first_h):
+def calc_cond_var(alpha_0, alpha, beta, y_squared, first_h,
+                  fuzzy=False, weights=None):
     logger = logging.getLogger('calc_cond_var')
+
+    if fuzzy and weights is None:
+        raise Exception('fuzzy is True, but weights not provided')
+
+    def _calc_ht(_alpha_0, _alpha, _beta, _y_squared, _h):
+        if fuzzy:
+            fun = calc_fuzzy_ht(_alpha_0, _alpha, _beta, _y_squared, _h, weights)
+        else:
+            fun = calc_ht(_alpha_0, _alpha, _beta, _y_squared, _h)
+
+        return fun
 
     q = len(alpha)
     p = len(beta)
@@ -32,21 +44,18 @@ def calc_cond_var(alpha_0, alpha, beta, y, first_h):
     starting_index = max(p, q)
 
     if len(first_h) < starting_index:
-        logger.error(f'Not enough first elements of h are set. p = {p}, q = {q}, max(p, q) = {starting_index}. '
-                     f'Therefore, we need first {starting_index} elements of h known. '
-                     f'However only {len(first_h)} are given: given first_h = {first_h}. '
-                     f'Returning None')
-        return
+        raise Exception(f'Not enough first elements of h are given. p = {p}, q = {q}, max(p, q) = {starting_index}. '
+                        f'Therefore, we need first {starting_index} elements of h known. '
+                        f'However only {len(first_h)} are given: given first_h = {first_h}')
 
     h = list(first_h)
 
-    y_len = len(y)
-    y_squared = y ** 2
+    y_len = len(y_squared)
 
     for i in range(starting_index, y_len):
         y_slc = slice(i - q, i)
         h_slc = slice(i - p, i)
-        h_t = calc_ht(alpha_0, alpha, beta, y_squared[y_slc], h[h_slc])
+        h_t = _calc_ht(alpha_0, alpha, beta, y_squared[y_slc], h[h_slc])
         h.append(h_t)
 
     return h
