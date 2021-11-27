9a3e964a7d54ac822b561852d8083ae895582d6d
Vladimir Sviyazov <sva125125@gmail.com>
2021-11-27 17:50:34 +0300

    [Feature] Implemented a test for fuzzy garch (algebraic calculation)

--- local_models/test_garch_ht_calculation.py
+++ local_models/test_garch_ht_calculation.py
@@ -3,7 +3,7 @@ from scipy.stats import norm
 from numpy import array
 import logging
 
-from . import calc_cond_var, calc_ht
+from . import calc_cond_var, calc_ht, calc_fuzzy_ht
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -13,22 +13,19 @@ class TestCustomGARCHCondVarCalc(unittest.TestCase):
         logger = logging.getLogger('test_vanilla_garch_1_1')
 
         # generating white noise
-        sample_size = 100
+        sample_size = 1
         mean = 0
         variance = 1
         data = norm(mean, variance ** .5).rvs(sample_size)
 
-        n = data.shape[0]
-
         # garch parameters
-        p = 1
-        q = 1
         alpha_0 = 1
         alpha = array([2])
         beta = array([4])
-        y_squared = array([6])
+        y_squared = data ** 2
         first_h = array([1])
 
+        # calculating
         h_main_fun = calc_cond_var(alpha_0, alpha, beta, y_squared, first_h,
                                    fuzzy=False, weights=None)
         h_aux_fun = calc_ht(alpha_0, alpha, beta, y_squared, first_h)
@@ -36,7 +33,37 @@ class TestCustomGARCHCondVarCalc(unittest.TestCase):
                      f'first_h = {first_h}')
         logger.debug(f'h_main_fun = {h_main_fun}')
 
-        self.assertEqual(h_main_fun[1], h_aux_fun)
+        tolerance = 1e-5
+        self.assertAlmostEqual(h_main_fun[1], h_aux_fun, delta=tolerance)
+
+    def test_fuzzy_garch_1_1(self):
+        logger = logging.getLogger('test_fuzzy_garch_1_1')
+
+        # generating white noise
+        sample_size = 1
+        mean = 0
+        variance = 1
+        data = norm(mean, variance ** .5).rvs(sample_size)
+
+        # garch parameters
+        alpha_0 = array([1])
+        alpha = array([[2]])
+        beta = array([[4]])
+        y_squared = data ** 2
+        first_h = array([1])
+
+        weights = array([1])
+
+        # calculating
+        h_main_fun = calc_cond_var(alpha_0, alpha, beta, y_squared, first_h,
+                                   fuzzy=True, weights=weights)
+        h_aux_fun = calc_fuzzy_ht(alpha_0, alpha, beta, y_squared, first_h, weights)
+        logger.debug(f'alpha_0 = {alpha_0}, alpha = {alpha}, beta = {beta}, y_squared = {y_squared}, '
+                     f'first_h = {first_h}')
+        logger.debug(f'h_main_fun = {h_main_fun}')
+
+        tolerance = 1e-5
+        self.assertAlmostEqual(h_main_fun[1], h_aux_fun, delta=tolerance)
 
 
 if __name__ == '__main__':
