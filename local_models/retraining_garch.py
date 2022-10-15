06083e64f4c49c966402ff75cd03dbe064a6e277
Vladimir Sviyazov <sva125125@gmail.com>
2022-10-15 20:08:53 +0300

    [Feature] n last points for fitting in retraining GARCH

--- local_models/retraining_garch.py
+++ local_models/retraining_garch.py
@@ -3,13 +3,25 @@ from arch import arch_model
 
 
 def calculate_retraining_garch_forecasts(train, test,
-                                         mean='Constant', vol='GARCH', p=1, q=1, dist='normal'):
+                                         mean='Constant', vol='GARCH', p=1, q=1, dist='normal',
+                                         n_points_fitting: int = None):
+    _fitting_slice = slice(-n_points_fitting if n_points_fitting is not None else None, None)
+
     train = train.copy()
     test = test.copy()
 
-    forecast = []
+    garch = arch_model(train[_fitting_slice],
+                       mean=mean,
+                       vol=vol,
+                       p=p,
+                       q=q,
+                       dist=dist)
+    garch_fitted = garch.fit()
+    one_step_forecast = garch_fitted.forecast(horizon=1, reindex=False).variance.iloc[0].values[0]
 
-    for date in test.index:
+    forecast = [one_step_forecast]
+
+    for date in test.index[:-1]:
         observation = test.loc[date]
         train.loc[date] = observation
 
@@ -17,7 +29,7 @@ def calculate_retraining_garch_forecasts(train, test,
         garch_mean = 'Zero'
         garch_dist = 'normal'
 
-        garch = arch_model(train,
+        garch = arch_model(train[_fitting_slice],
                            mean=mean,
                            vol=vol,
                            p=p,
