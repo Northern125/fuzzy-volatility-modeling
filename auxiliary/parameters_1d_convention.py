f7abba2206d2473d9d946e3fdd385d8613c4f6f7
Vladimir Sviyazov <sva125125@gmail.com>
2023-04-20 23:52:02 +0300

    [Feature] Params pack / unpack convention
    Implemented new functions for packing / unpacking params into / from 1D array for a CBC (cluster by cluster) convention

--- auxiliary/parameters_1d_convention.py
+++ auxiliary/parameters_1d_convention.py
@@ -1,4 +1,4 @@
-from numpy import array
+from numpy import array, concatenate
 
 
 def unpack_1d_parameters(parameters, p, q, n_clusters):
@@ -16,3 +16,43 @@ def pack_1d_parameters(alpha_0, alpha, beta):
     parameters = array(parameters)
 
     return parameters
+
+
+def unpack_1d_params_cbc(params: array,
+                         p: int,
+                         q: int,
+                         n_clusters: int
+                         ) -> tuple[array, array, array]:
+    """
+    'cbc' stands for 'cluster by cluster'
+    :param params: 1D numpy.array. Cluster-by-cluster stacked parameters
+    :param p: int
+    :param q: int
+    :param n_clusters: int
+    :return: tuple(alpha_0, alpha, beta)
+    """
+    n_params_in_a_rule = p + q + 1
+
+    params_by_clusters = array([params[(i - 1) * n_params_in_a_rule:i * n_params_in_a_rule]
+                                for i in range(1, n_clusters + 1)]).copy()
+
+    alpha_0 = array([_params[0] for _params in params_by_clusters]).copy()
+    alpha = array([_params[1:q + 1] for _params in params_by_clusters]).T.copy()
+    beta = array([_params[q + 1:] for _params in params_by_clusters]).T.copy()
+
+    return alpha_0, alpha, beta
+
+
+def pack_1d_params_cbc(alpha_0, alpha, beta):
+    """
+
+    :param alpha_0:
+    :param alpha:
+    :param beta:
+    :return:
+    """
+    n_clusters: int = len(alpha_0)
+
+    params = array([concatenate(([alpha_0[i]], alpha[:, i], beta[:, i])) for i in range(n_clusters)]).flatten()
+
+    return params
