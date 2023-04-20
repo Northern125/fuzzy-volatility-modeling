c7d5d564190e06baac992c584b37ef0de6953c66
Vladimir Sviyazov <sva125125@gmail.com>
2023-04-20 17:34:07 +0300

    [Feature] RLS new cluster re-estimation
    Implemented a re-estimation function for the case when a new cluster is added, synced with the eTS model

--- consequent_estimators/recursive_ls.py
+++ consequent_estimators/recursive_ls.py
@@ -1,11 +1,11 @@
-from numpy import array, reshape
+from numpy import array, reshape, dot, concatenate, eye, block, zeros
 
 
 def update_parameters(params_prev: array,
                       cov_prev: array,
                       y_new: float,
                       coeffs_prev: array
-                      ) -> tuple:
+                      ) -> tuple[array, array]:
     """
     The equation is as follows: `y_{t + 1} = psi_t^T * theta_t`. At time point `t` we have `y_t`, `psi_{t-1}`,
     `theta_{t-1}`, `C_{t-1}` and we estimate `theta_t`.
@@ -26,5 +26,43 @@ def update_parameters(params_prev: array,
 
     params_new = params_prev + cov_new @ coeffs_prev * (y_new - coeffs_prev.T @ params_prev)
     params_new = params_new.flatten().copy()
+
+    return cov_new, params_new
+
+
+def ets_new_cluster_update_parameters(params_prev: array,
+                                      cov_prev: array,
+                                      weights: array,
+                                      n_params_in_a_rule: int,
+                                      omega: float
+                                      ) -> tuple[array, array]:
+    """
+
+    :param params_prev:
+    :param cov_prev:
+    :param: weights
+    :param n_params_in_a_rule
+    :param omega
+    :return:
+    """
+    weights = (weights / weights.sum()).copy()
+
+    if len(params_prev) % n_params_in_a_rule != 0:
+        raise ValueError('The length of `params_prev` should be a multiple of `n_params_in_a_rule`; got '
+                         f'`params_prev = {params_prev}`, `n_params_in_a_rule = {n_params_in_a_rule}`')
+
+    n_clusters = int(len(params_prev) / n_params_in_a_rule)
+    params_by_clusters = array([params_prev[(i - 1) * n_params_in_a_rule:i * n_params_in_a_rule]
+                                for i in range(1, n_clusters + 1)]).copy()
+
+    # parameters
+    new_cluster_params = dot(weights, params_by_clusters).copy()
+    params_new = concatenate((params_prev, new_cluster_params)).copy()
+
+    # covariance matrix
+    new_cluster_cov = (eye(n_params_in_a_rule) * omega).copy()
+    cov_new = block([[cov_prev, zeros((cov_prev.shape[0], new_cluster_cov.shape[1]))],
+                     [zeros((new_cluster_cov.shape[0], cov_prev.shape[1])), new_cluster_cov]
+                     ]).copy()
 
     return cov_new, params_new
