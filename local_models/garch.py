7085429db50a39685d1f8378be473d96eb2d8c56
Vladimir Sviyazov <sva125125@gmail.com>
2023-05-27 18:46:20 +0300

    [Doc] Docstrings in module garch.py

--- local_models/garch.py
+++ local_models/garch.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from rules_related import combine_rules_outputs
 from numpy import array, apply_along_axis
 import logging
@@ -11,6 +13,17 @@ def calc_ht(alpha_0: float,
             beta: array,
             y_squared: array,
             h: array) -> float:
+    """
+    Calculate vanilla (non-fuzzy) heteroskedasticity value at step t given GARCH parameters & y^2 and h lagged values.
+    `y_squared` and `h` are passed in the reverse order!
+
+    :param alpha_0: float. GARCH parameter
+    :param alpha: 1D numpy.array of length `q`. GARCH parameter
+    :param beta: 1D numpy.array of length `p`. GARCH parameter
+    :param y_squared: 1D numpy.array of shape (`len(alpha)`, ). Last `q` lagged y^2 values in reverse order
+    :param h: 1D numpy.array if shape (`len(beta)`, ). Last `p` lagged conditional variance values in reverse order
+    :return: float. Heteroskedasticity `h_t` value at current time step
+    """
     res = alpha_0 + (alpha * y_squared).sum() + (beta * h).sum()
     return res
 
@@ -21,17 +34,21 @@ def calc_fuzzy_ht_aggregated(alpha_0: array,
                              y_squared: array,
                              h: array,
                              weights: array,
-                             ) -> array:
+                             ) -> float:
     """
+    Calculate fuzzy heteroskedasticity aggregated value at step t given GARCH parameters & y^2 and h lagged values.
+    Aggregated means `h` lags used in the GARCH formula are aggregated, not rule-wise.
+    `y_squared` and `h` are passed in the reverse order!
 
-    :param alpha_0:
-    :param alpha:
-    :param beta:
-    :param y_squared:
-    :param h: 1D or 2D numpy.array, depending on `past_h_type`. The past conditional variance values `h_{t-j}`
-    :param weights:
-    aggregated h values. If 'rule-wise': `h` should be 2 dimensional and contain rule-wise `h_{t-j}^(k)` values
-    :return: 1D numpy.array. Aggregated output of all clusters
+    :param alpha_0: 1D numpy.array of shape `(n_clusters, )`. GARCH parameter for each cluster
+    :param alpha: 2D numpy.array of shape `(q, n_clusters)`. GARCH parameter for each cluster
+    :param beta: 2D numpy.array of shape `(p, n_clusters)`. GARCH parameter for each cluster
+    :param y_squared: 1D numpy.array of shape (`len(alpha)`, ). Last `q` lagged y^2 values in reverse order
+    :param h: 1D numpy.array of shape (`len(beta)`, ). The AGGREGATED `p` past conditional variance values `h_{t-j}` in
+    reverse order
+    :param weights: 1D numpy.array of length `n_clusters`. Weight of each cluster (for output aggregation).
+    `n_clusters` is inferred from this parameter
+    :return: float. Aggregated conditional variance `h_t` value at current time step
     """
     n_clusters: int = len(weights)
 
@@ -54,14 +71,19 @@ def calc_fuzzy_ht_rule_wise(alpha_0: array,
                             n_clusters: int
                             ) -> array:
     """
+    Calculate fuzzy heteroskedasticity aggregated value at step t given GARCH parameters & y^2 and h lagged values.
+    Rule-wise `h` lags are used in the GARCH formula (own `h` for each fuzzy rule).
+    `y_squared` and `h` are passed in the reverse order!
 
-    :param alpha_0:
-    :param alpha:
-    :param beta:
-    :param y_squared:
-    :param h:
-    :param n_clusters:
-    :return:
+    :param alpha_0: 1D numpy.array of shape `(n_clusters, )`. GARCH parameter for each cluster
+    :param alpha: 2D numpy.array of shape `(q, n_clusters)`. GARCH parameter for each cluster
+    :param beta: 2D numpy.array of shape `(p, n_clusters)`. GARCH parameter for each cluster
+    :param y_squared: 1D numpy.array of length `q`. Last `q` lagged y^2 values in reverse order
+    :param h: 2D numpy.array of shape `(p, n_clusters)`. Last `p` conditional variance values `h_{t-j}^(k)` in reverse
+    order FOR EACH CLUSTER
+    :param n_clusters: int. Number of clusters
+    :return: 1D numpy.array of length `n_clusters`. Conditional variance `h_t^(k)` value at current time step for
+    each cluster
     """
     outputs = [
         calc_ht(alpha_0[j], alpha[:, j], beta[:, j], y_squared, h[:, j])
@@ -72,9 +94,31 @@ def calc_fuzzy_ht_rule_wise(alpha_0: array,
     return outputs
 
 
-def _calc_cond_var(alpha_0, alpha, beta, y_squared, first_h,
+def _calc_cond_var(alpha_0: Union[float, array],
+                   alpha: array,
+                   beta: array,
+                   y_squared,
+                   first_h,
                    calc_ht_function: callable = calc_ht,
                    **kwargs) -> array:
+    """
+    Calculate conditional variance recursively from the beginning of the `y_squared` series. Depending on the
+    `calc_ht_function` argument, the calculation method can be either a vanilla GARCH, a fuzzy GARCH w/ aggregated `h`
+    or a fuzzy GARCH w/ rule-wise `h`
+
+    :param alpha_0: float for vanilla GARCH, 1D numpy.array of len `n_clusters` for fuzzy GARCH
+    :param alpha: 1D numpy.array of len `q` for vanilla GARCH, 2D numpy.array of shape `(q, n_clusters)` for fuzzy GARCH
+    :param beta: 1D numpy.array of len `q` for vanilla GARCH, 2D numpy.array of shape `(q, n_clusters)` for fuzzy GARCH
+    :param y_squared: 1D numpy.array of any length
+    :param first_h: 1D numpy.array of len `p` for vanilla GARCH & fuzzy GARCH w/ aggregated `h`,
+    2D numpy.array of shape `(p, n_clusters)` for fuzzy GARCH w/ rule-wise (fuzzy) `h`.
+    The first `p` values of the conditional variance
+    :param calc_ht_function: callable, one of (calc_ht, calc_fuzzy_ht_aggregated, calc_fuzzy_ht_rule_wise)
+    :param kwargs: kwargs for `calc_ht_function`
+    :return: 1D numpy.array of length `len(y_squared) + 1` for vanilla GARCH & fuzzy GARCH w/ aggregated `h`
+    and 2D numpy array of shape `(len(y_squared) + 1, n_clusters)` for fuzzy GARCH w/ rule-wise (fuzzy) `h`.
+    The recursively calculated conditional variance values from time 0 up to time step t + 1
+    """
     logger = logging.getLogger('_calc_cond_var')
 
     q = len(alpha)
@@ -108,14 +152,57 @@ def _calc_cond_var(alpha_0, alpha, beta, y_squared, first_h,
     return h
 
 
-def calc_cond_var_vanilla(alpha_0, alpha, beta, y_squared, first_h) -> array:
+def calc_cond_var_vanilla(alpha_0: float,
+                          alpha: array,
+                          beta: array,
+                          y_squared: array,
+                          first_h: array
+                          ) -> array:
+    """
+    A wrapper function for conditional variance calculation for vanilla GARCH.
+    Calculate conditional variance recursively from the beginning of the `y_squared` series.
+
+    :param alpha_0: float. GARCH parameter
+    :param alpha: 1D numpy.array of length `q`. GARCH parameter
+    :param beta: 1D numpy.array of length `p`. GARCH parameter
+    :param y_squared: 1D numpy.array of any length
+    :param first_h: 1D numpy.array of len `p` for vanilla GARCH. The first `p` values of the conditional variance
+    :return: 1D numpy.array of length `len(y_squared) + 1`.
+    The recursively calculated conditional variance values from time 0 up to time step t + 1
+    """
     return _calc_cond_var(alpha_0, alpha, beta, y_squared, first_h,
                           calc_ht_function=calc_ht)
 
 
-def calc_cond_var_fuzzy(alpha_0, alpha, beta, y_squared, first_h, weights,
+def calc_cond_var_fuzzy(alpha_0: array,
+                        alpha: array,
+                        beta: array,
+                        y_squared: array,
+                        first_h: array,
+                        weights: array,
                         return_fuzzy: bool = False,
-                        past_h_type: str = PAST_H_TYPE_DEFAULT) -> array:
+                        past_h_type: str = PAST_H_TYPE_DEFAULT
+                        ) -> Union[tuple[array, array], array]:
+    """
+    A wrapper function for conditional variance calculation for fuzzy GARCH.
+    Calculate conditional variance recursively from the beginning of the `y_squared` series
+
+    :param alpha_0: 1D numpy.array of shape `(n_clusters, )`. GARCH parameter for each cluster
+    :param alpha: 2D numpy.array of shape `(q, n_clusters)`. GARCH parameter for each cluster
+    :param beta: 2D numpy.array of shape `(p, n_clusters)`. GARCH parameter for each cluster
+    :param y_squared: 1D numpy.array of any length
+    :param first_h: The first `p` values of the conditional variance `h`.
+    If `past_h_type == rule-wise`: 2D numpy.array of shape `(p, n_clusters)`; contains rule-wise values.
+    If `past_h_type == aggregated`: 1D numpy.array of len `p`; contains aggregated values
+    :param weights: 1D numpy.array of length `n_clusters`. Weight of each cluster (for output aggregation)
+    :param return_fuzzy: bool. If True, the function also returns fuzzy conditional variance
+    :param past_h_type: str. Type of past conditional variance ('rule-wise' AKA fuzzy or 'aggregated')
+    :return: If `return_fuzzy == True`: `tuple(fuzzy_h, aggr_h)`.
+    If `return_fuzzy == False`: `aggr_h`.
+    `aggr_h` is a 1D numpy.array of length `len(y_squared) + 1`, aggregated conditional variance.
+    `fuzzy_h` is a 2D numpy.array of shape `(len(y_squared) + 1, n_clusters)`, fuzzy conditional variance.
+    The recursively calculated conditional variance values from time 0 up to time step t + 1
+    """
     logger = logging.getLogger('calc_cond_var_fuzzy')
 
     n_clusters: int = weights.shape[0]
